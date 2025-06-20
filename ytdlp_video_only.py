27b6eb29e26c38dca19efc594f6bdcbaa470d196
GhostKX <171037100+GhostKX@users.noreply.github.com>
2025-06-20 13:38:50 +0500

    Updated Video Only logic

--- ytdlp_video_only.py
+++ ytdlp_video_only.py
@@ -1,8 +1,12 @@
 import yt_dlp
+import subprocess
+
 
 # 🔗 Ask the user for the YouTube video URL
 url = input("🎥 Enter YouTube video URL: ").strip()
 
+output_path = 'video_only.mp4'
+fixed_path = 'video_only_fixed.mp4'
 
 # ⚙️ yt_dlp configuration
 ydl_opts = {
@@ -15,9 +19,22 @@ ydl_opts = {
 try:
     print("⬇️ Downloading H.264 MP4 video...\n")
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+        download_path = ydl.download([url])
+
+    # Remux video to fix timestamps, without re-encoding
+    cmd = [
+        "ffmpeg",
+        "-i", output_path ,
+        "-c", "copy",
+        "-fflags", "+genpts",
+        fixed_path,
+        "-y"
+    ]
+
+    subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
     print("\n✅ Download completed successfully!")
-    print("📂 Saved as: test.mp4")
+    print("📂 Saved as: video_only_fixed.mp4")
 
 except Exception as e:
     print(f"\n❌ An error occurred:\n{e}")
